4a4ae7bfd89d68fd43e4a6687a6d6c8c7ee9ed57
1ejun6 <lowyinjin@gmail.com>
2025-09-05 17:29:50 +0800

    u: navbar and feature pages

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -4,11 +4,7 @@ import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
 
-export default function ProtectedLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function ProtectedLayout({children,}: {children: React.ReactNode;}) {
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
---
+++ app/protected/melting-pot/page.tsx
@@ -0,0 +1,5 @@
+export default function MeltingPotPage() {
+  return (
+        <p>Melting Pot</p>
+  );
+}
