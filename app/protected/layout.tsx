07345bc7c0244803c246a874c350ad436b0b346c
jungfraujoch0 <sweeminnn@gmail.com>
2025-09-06 10:09:25 +0800

    edit team name

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -11,7 +11,7 @@ export default function ProtectedLayout({children,}: {children: React.ReactNode;
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Nerd Cluster</Link>
+              <Link href={"/"}>Geek Cluster</Link>
             </div>
             {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
           </div>
